1ee14b7d1e3e2b783bbf876756dd156b0e0feae6
RedRe4per <s3582474@gmail.com>
2023-03-03 17:19:40 +1100

    feat: svg update, tailwind layer/component

--- layouts/nav/index.tsx
+++ layouts/nav/index.tsx
@@ -10,34 +10,40 @@ export const Nav = () => {
     <main className="w-[330px] bg-gray-800">
       <section className="text-h3-light mt-[50px] flex flex-col gap-[7px]">
         <div
-          className={`nav-title ${
-            activePageIndex === 1
-              ? "bg-green bg-opacity-15"
-              : "hover:bg-gray-600"
-          }`}
+          className={`nav-title ${activePageIndex === 1
+            ? "bg-green bg-opacity-15"
+            : "hover:bg-gray-600"
+            }`}
           onClick={() => handlePage(1)}
         >
-          <h3 className="ml-[82px] pt-[14px]">Home</h3>
+          <div className="ml-[23px] w-[40px]">
+            <img src="/icons/home.svg" alt="home icon" className="m-auto" />
+          </div>
+          <h3 className="ml-[20px]">Home</h3>
         </div>
         <div
-          className={`nav-title ${
-            activePageIndex === 2
-              ? "bg-green bg-opacity-15"
-              : "hover:bg-gray-600"
-          }`}
+          className={`nav-title ${activePageIndex === 2
+            ? "bg-green bg-opacity-15"
+            : "hover:bg-gray-600"
+            }`}
           onClick={() => handlePage(2)}
         >
-          <h3 className="ml-[82px] pt-[14px]">Browse</h3>
+          <div className="ml-[23px] w-[40px]">
+            <img src="/icons/browse.svg" alt="home icon" className="m-auto" />
+          </div>
+          <h3 className="ml-[20px]">Browse</h3>
         </div>
         <div
-          className={`nav-title ${
-            activePageIndex === 3
-              ? "bg-green bg-opacity-15"
-              : "hover:bg-gray-600"
-          }`}
+          className={`nav-title ${activePageIndex === 3
+            ? "bg-green bg-opacity-15"
+            : "hover:bg-gray-600"
+            }`}
           onClick={() => handlePage(3)}
         >
-          <h3 className="ml-[82px] pt-[14px]">Liked songs</h3>
+          <div className="ml-[23px] w-[40px]">
+            <img src="/icons/heart-line.svg" alt="home icon" className="m-auto scale-90" />
+          </div>
+          <h3 className="ml-[20px]">Liked songs</h3>
         </div>
       </section>
       <section className="mt-[37px] ml-[40px] text-gray-200">
