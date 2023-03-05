a00054d69dd6c9000a87060f70f81b75e1f9cfc8
RedRe4per <s3582474@gmail.com>
2023-03-05 22:32:44 +1100

    feat: nav style complete

--- components/NavPlaylist/index.tsx
+++ components/NavPlaylist/index.tsx
@@ -3,20 +3,22 @@ import { PlaylistItem } from "./PlaylistItem";
 const listItemArray = ["*", "Chill", "Lofi"];
 
 export const NavPlaylist = () => {
-    return (
-        <section className="mt-[40px] ml-[20px] text-white-200 text-gray-200 text-mix-blend-hard-light">
-            <h5 className="text-tag-normal uppercase ml-[20px]">Playlists</h5>
-            <ul className="mt-[20px] flex flex-col gap-[5px]">
-                <li className="flex items-center w-[250px] h-[46px] rounded-[10px] bg-gray-600">
-                    <img src="/icons/plus.svg" alt="plus" className="h-[17px] ml-[20px]" />
-                    <span className="uppercase ml-[17px]">new playlist</span>
-                </li>
-                {
-                    listItemArray.map((listItem) => {
-                        return <PlaylistItem key={listItem} text={listItem} />
-                    })
-                }
-            </ul>
-        </section>
-    );
+  return (
+    <section className="mt-[40px] ml-[20px] text-white-200 text-gray-200 text-mix-blend-hard-light">
+      <h5 className="text-tag-normal uppercase ml-[20px]">Playlists</h5>
+      <ul className="mt-[20px] flex flex-col gap-[5px]">
+        <li className="flex items-center w-[250px] h-[46px] rounded-[10px] bg-gray-600">
+          <img
+            src="/icons/plus.svg"
+            alt="plus"
+            className="h-[17px] ml-[20px]"
+          />
+          <span className="uppercase ml-[17px]">new playlist</span>
+        </li>
+        {listItemArray.map((listItem) => {
+          return <PlaylistItem key={listItem} text={listItem} />;
+        })}
+      </ul>
+    </section>
+  );
 };
