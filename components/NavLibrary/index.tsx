66379b1a47863c4969d57c0b977b55330bfa63fd
RedRe4per <s3582474@gmail.com>
2023-03-06 02:45:10 +1100

    fix: font color mix remove

--- components/NavLibrary/index.tsx
+++ components/NavLibrary/index.tsx
@@ -1,8 +1,9 @@
 import { LibraryItem } from "./LibraryItem";
 
 export const NavLibrary = () => {
+  //text-white-200 text-gray-200 mix-blend-hard-light
   return (
-    <section className="mt-[45px] ml-[40px] text-white-200 text-gray-200 mix-blend-hard-light">
+    <section className="mt-[45px] ml-[40px] text-white-200">
       <h5 className="text-tag-normal uppercase">Your library</h5>
       <ul className="text-h3-bold flex flex-col gap-[20px] mt-[17px]">
         <LibraryItem text="Made for you" />
