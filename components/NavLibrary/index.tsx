35d89b5843bfd5c043ec0eb4c687a1475894c345
RedRe4per <s3582474@gmail.com>
2023-03-05 22:32:18 +1100

    feat: nav style complete

--- components/NavLibrary/index.tsx
+++ components/NavLibrary/index.tsx
@@ -1,17 +1,17 @@
 import { LibraryItem } from "./LibraryItem";
 
 export const NavLibrary = () => {
-  return (
-    <section className="mt-[37px] ml-[40px] text-white-200 text-gray-200 mix-blend-hard-light">
-      <span className="text-tag-normal uppercase">Your library</span>
-      <ul className="text-h3-bold flex flex-col gap-[20px] mt-[17px]">
-        <LibraryItem text="Made for you" />
-        <LibraryItem text="Recent player" />
-        <LibraryItem text="Albums" />
-        <LibraryItem text="Artists" />
-        <LibraryItem text="Local files" />
-        <LibraryItem text="Podcasts" />
-      </ul>
-    </section>
-  );
+    return (
+        <section className="mt-[45px] ml-[40px] text-white-200 text-gray-200 mix-blend-hard-light">
+            <h5 className="text-tag-normal uppercase">Your library</h5>
+            <ul className="text-h3-bold flex flex-col gap-[20px] mt-[17px]">
+                <LibraryItem text="Made for you" />
+                <LibraryItem text="Recent player" />
+                <LibraryItem text="Albums" />
+                <LibraryItem text="Artists" />
+                <LibraryItem text="Local files" />
+                <LibraryItem text="Podcasts" />
+            </ul>
+        </section>
+    );
 };
--- layouts/nav/index.tsx
+++ layouts/nav/index.tsx
@@ -1,11 +1,13 @@
 import { NavPages } from "@/components/NavPages";
 import { NavLibrary } from "@/components/NavLibrary";
+import { NavPlaylist } from "@/components/NavPlaylist";
 
 export const Nav = () => {
   return (
     <main className="w-[330px] bg-gray-800 h-[100vh] absolute">
       <NavPages />
       <NavLibrary />
+      <NavPlaylist />
     </main>
   );
 };
