2e086cccc1b3910949edb2a5c6a379cac74b6bf1
RedRe4per <s3582474@gmail.com>
2023-03-08 03:58:09 +1100

    fix: progress slider drag rerender

--- components/MusicPlayerBar/ProgressBar.tsx
+++ components/MusicPlayerBar/ProgressBar.tsx
@@ -3,77 +3,77 @@ import { formatTime } from "@/utils/formatTime";
 import { MusicDetail } from "@/interfaces/music";
 
 interface Props {
-    currentMusic: MusicDetail | null;
-    isMusicLoop: boolean;
-    handleSkipMusic: (param: "next" | "last") => void;
+  currentMusic: MusicDetail | null;
+  isMusicLoop: boolean;
+  handleSkipMusic: (param: "next" | "last") => void;
 }
 
 export const ProgressBar = React.forwardRef(
-    ({ currentMusic, isMusicLoop, handleSkipMusic }: Props, musicPlayer: any) => {
-        //React.ForwardedRef<HTMLAudioElement>
-        const [currentPlayRadio, setCurrentPlayRadio] = useState(0);
-        const [currentMusicTime, setCurrentMusicTime] = useState<number>(0);
-        const [currentDurationTime, setCurrentDurationTime] = useState<number>(0);
-        const [intervalId, setIntervalId] = useState<number | NodeJS.Timeout>(0);
+  ({ currentMusic, isMusicLoop, handleSkipMusic }: Props, musicPlayer: any) => {
+    //React.ForwardedRef<HTMLAudioElement>
+    const [currentPlayRadio, setCurrentPlayRadio] = useState(0);
+    const [currentMusicTime, setCurrentMusicTime] = useState<number>(0);
+    const [currentDurationTime, setCurrentDurationTime] = useState<number>(0);
+    const [intervalId, setIntervalId] = useState<number | NodeJS.Timeout>(0);
 
-        const handlePlay = () => {
-            setIntervalId(
-                setInterval(() => {
-                    setCurrentMusicTime(musicPlayer.current?.currentTime);
-                    setCurrentDurationTime(musicPlayer.current?.duration);
-                    setCurrentPlayRadio(
-                        Math.round(
-                            musicPlayer.current?.currentTime && musicPlayer.current?.duration
-                                ? (100 * musicPlayer.current?.currentTime) /
-                                musicPlayer.current?.duration
-                                : 0
-                        )
-                    );
-                }, 1000)
-            );
-        };
+    const handlePlay = () => {
+      setIntervalId(
+        setInterval(() => {
+          setCurrentMusicTime(musicPlayer.current?.currentTime);
+          setCurrentDurationTime(musicPlayer.current?.duration);
+          setCurrentPlayRadio(
+            Math.round(
+              musicPlayer.current?.currentTime && musicPlayer.current?.duration
+                ? (100 * musicPlayer.current?.currentTime) /
+                    musicPlayer.current?.duration
+                : 0
+            )
+          );
+        }, 1000)
+      );
+    };
 
-        const handleEnd = () => {
-            handleSkipMusic("next");
+    const handleEnd = () => {
+      handleSkipMusic("next");
+      clearInterval(intervalId);
+    };
+
+    const handleProgress = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCurrentPlayRadio(+e.target.value);
+      setCurrentMusicTime(musicPlayer.current?.currentTime);
+      musicPlayer.current!.currentTime =
+        musicPlayer.current!.duration * (+e.target.value / 100);
+    };
+
+    return (
+      <section>
+        <audio
+          ref={musicPlayer}
+          src={currentMusic?.url}
+          loop={isMusicLoop}
+          onPlay={handlePlay}
+          onPause={() => {
             clearInterval(intervalId);
-        };
-
-        const handleProgress = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setCurrentPlayRadio(+e.target.value);
-            setCurrentMusicTime(musicPlayer.current?.currentTime);
-            musicPlayer.current!.currentTime =
-                musicPlayer.current!.duration * (+e.target.value / 100);
-        };
-
-        return (
-            <section>
-                <audio
-                    ref={musicPlayer}
-                    src={currentMusic?.url}
-                    loop={isMusicLoop}
-                    onPlay={handlePlay}
-                    onPause={() => {
-                        clearInterval(intervalId);
-                    }}
-                    onEnded={handleEnd}
-                />
-                <div className="w-[800px] h-[10px] flex justify-around items-center my-2">
-                    <div className="w-[10%] text-center">
-                        <span>{formatTime(currentMusicTime)}</span>
-                    </div>
-                    <input
-                        type="range"
-                        min={0}
-                        max={100}
-                        value={currentPlayRadio}
-                        onChange={handleProgress}
-                        className="appearance-none cursor-pointer rounded-lg accent-gray-200 w-[80%] h-[5px]"
-                    />
-                    <div className="w-[10%] text-center">
-                        <span>{formatTime(currentDurationTime)}</span>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+          }}
+          onEnded={handleEnd}
+        />
+        <div className="w-[800px] h-[10px] flex justify-around items-center my-2">
+          <div className="w-[10%] text-center">
+            <span>{formatTime(currentMusicTime)}</span>
+          </div>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={currentPlayRadio}
+            onChange={handleProgress}
+            className="appearance-none cursor-pointer rounded-lg accent-gray-200 w-[80%] h-[5px]"
+          />
+          <div className="w-[10%] text-center">
+            <span>{formatTime(currentDurationTime)}</span>
+          </div>
+        </div>
+      </section>
+    );
+  }
 );
