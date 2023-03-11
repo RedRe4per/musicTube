f8bb1637e41c98732aaa34f064cbe3ec24ac963f
RedRe4per <s3582474@gmail.com>
2023-03-12 08:33:00 +1100

    feat: favicon and title

--- components/AlbumList/index.tsx
+++ components/AlbumList/index.tsx
@@ -8,7 +8,6 @@ interface Props {
 export const AlbumList = ({ title, albumList }: Props) => {
   return (
     <section className="ml-6 mt-6">
-      {" "}
       {/*overflow-y-hidden*/}
       <h2 className="text-h3-normal text-white-200">{title}</h2>
       <section className="mt-3 flex gap-6 w-[350vw] sm:w-[310vw] md:w-[270vw] lg:w-[220vw] xl:w-[170vw] 2xl:w-[130vw] custom:w-[100vw]">
