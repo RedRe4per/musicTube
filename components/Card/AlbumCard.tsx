45fa03d9b566e677831f2ff4cb9a11458467594e
RedRe4per <s3582474@gmail.com>
2023-03-11 18:54:21 +1100

    feat: image loading optimization

--- components/Card/AlbumCard.tsx
+++ components/Card/AlbumCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Props {
   albumUrl: string;
   albumName: string;
@@ -7,10 +9,12 @@ interface Props {
 export const AlbumCard = ({ albumUrl, albumName, artists }: Props) => {
   return (
     <div className="w-[200px] h-[330px]">
-      <img
+      <Image
         src={albumUrl}
         alt={albumName}
         className="w-[200] h-[200] object-contain rounded-xl"
+        width={200}
+        height={200}
       />
       <h5 className="text-button-normal text-white-200 mt-[15px]">
         {albumName}
--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["p2.music.126.net"],
+    domains: [
+      'https://p1.music.126.net',
+      'https://p2.music.126.net',
+      'p1.music.126.net',
+      'p2.music.126.net'
+    ],
   },
 };
 
