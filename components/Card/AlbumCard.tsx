5ae4277f8e146b64c929346d9f968cfe686b27f0
RedRe4per <s3582474@gmail.com>
2023-03-12 20:15:02 +1100

    feat: dynamic routing for album page

--- components/Card/AlbumCard.tsx
+++ components/Card/AlbumCard.tsx
@@ -37,41 +37,41 @@ export const AlbumCard = ({ albumUrl, albumName, artists, albumId }: Props) => {
   return (
     <section className="max-w-[1/8] h-[250px] lg:h-[330px] relative overflow-hidden">
       <Link href={`/album/${albumId}`}>
-      <div className="relative">
-        <Image
-          src={albumUrl}
-          alt={albumName}
-          className="object-contain rounded-xl"
-          width={200}
-          height={200}
-        />
-        <div
-          onMouseEnter={() => setShowPlay(true)}
-          onMouseLeave={() => setShowPlay(false)}
-          className="absolute top-0 left-0 w-full h-full hover:bg-gray-300 opacity-50 rounded-xl flex items-center justify-center"
-        >
+        <div className="relative">
           <Image
-            src="/icons/play-in-image.svg"
-            alt="play"
-            className={`${
-              showPlay ? "" : "hidden"
-            } hover:w-[75px] animate-bounce cursor-pointer`}
-            width={70}
-            height={70}
-            onClick={handlePlay}
+            src={albumUrl}
+            alt={albumName}
+            className="object-contain rounded-xl"
+            width={200}
+            height={200}
           />
+          <div
+            onMouseEnter={() => setShowPlay(true)}
+            onMouseLeave={() => setShowPlay(false)}
+            className="absolute top-0 left-0 w-full h-full hover:bg-gray-300 opacity-50 rounded-xl flex items-center justify-center"
+          >
+            <Image
+              src="/icons/play-in-image.svg"
+              alt="play"
+              className={`${
+                showPlay ? "" : "hidden"
+              } hover:w-[75px] animate-bounce cursor-pointer`}
+              width={70}
+              height={70}
+              onClick={handlePlay}
+            />
+          </div>
         </div>
-      </div>
-      <section className="absolute max-w-[100%] overflow-hidden">
-        <h5 className="text-button-normal text-white-200 mt-[15px] ">
-          {albumName}
-        </h5>
-        <h6 className="text-tag-normal text-gray-400 mt-[7px] ">
-          {artists.map((artist, index) => {
-            return <span key={index}>{artist}&nbsp;&nbsp;</span>;
-          })}
-        </h6>
-      </section>
+        <section className="absolute max-w-[100%] overflow-hidden">
+          <h5 className="text-button-normal text-white-200 mt-[15px] ">
+            {albumName}
+          </h5>
+          <h6 className="text-tag-normal text-gray-400 mt-[7px] ">
+            {artists.map((artist, index) => {
+              return <span key={index}>{artist}&nbsp;&nbsp;</span>;
+            })}
+          </h6>
+        </section>
       </Link>
     </section>
   );
--- pages/album/[id].tsx
+++ pages/album/[id].tsx
@@ -1,29 +1,26 @@
 interface SSRProps {
-    albumId: number;
+  albumId: number;
 }
 
 interface Pros {
-    album: any;
+  album: any;
 }
 
 export default function Album({ album }: any) {
-    console.log(album, "album page")
+  console.log(album, "album page");
 
-
-
-
-    return (
-        <main>
-            <div>123123123</div>
-        </main>
-    )
+  return (
+    <main>
+      <div>123123123</div>
+    </main>
+  );
 }
 
-export async function getServerSideProps( context: any ) {
-    const { id } = context.query;
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/album?id=${id}`
-    );
-    const album = await response.json();
-    return { props: album };
+export async function getServerSideProps(context: any) {
+  const { id } = context.query;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/album?id=${id}`
+  );
+  const album = await response.json();
+  return { props: album };
 }
