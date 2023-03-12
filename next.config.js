8d62f2ad2ecd29eda7d9c01c05654e31b147b8eb
RedRe4per <s3582474@gmail.com>
2023-03-12 17:40:20 +1100

    fix: key duplication possibility

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["p1.music.126.net", "p2.music.126.net", "p3.music.126.net", "p4.music.126.net"],
+    domains: [
+      "p1.music.126.net",
+      "p2.music.126.net",
+      "p3.music.126.net",
+      "p4.music.126.net",
+    ],
   },
 };
 
