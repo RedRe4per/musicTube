d3fe2b9b609db4b5657087787331ed44b8c4029f
RedRe4per <s3582474@gmail.com>
2023-03-12 11:07:41 +1100

    feat: styled play button in image

--- components/Card/AlbumCard.tsx
+++ components/Card/AlbumCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 interface Props {
   albumUrl: string;
@@ -7,6 +8,8 @@ interface Props {
 }
 
 export const AlbumCard = ({ albumUrl, albumName, artists }: Props) => {
+  const [showPlay, setShowPlay] = useState(false);
+
   return (
     <section className="max-w-[1/8] h-[250px] lg:h-[330px] relative overflow-hidden">
       <div className="relative">
@@ -17,7 +20,16 @@ export const AlbumCard = ({ albumUrl, albumName, artists }: Props) => {
           width={200}
           height={200}
         />
-        <div className="absolute top-0 left-0 w-full h-full hover:bg-gray-300 opacity-50 rounded-xl"></div>
+        <div onMouseEnter={() => setShowPlay(true)} onMouseLeave={() => setShowPlay(false)} className="absolute top-0 left-0 w-full h-full hover:bg-gray-300 opacity-50 rounded-xl flex items-center justify-center">
+        <Image
+        src="/icons/play-in-image.svg"
+        alt="play"
+        className={`${showPlay? "":"hidden"} hover:w-[80px] animate-bounce`}
+        width={70}
+        height={70}
+        />
+
+        </div>
       </div>
       <section className="absolute max-w-[100%] overflow-hidden">
         <h5 className="text-button-normal text-white-200 mt-[15px] ">
