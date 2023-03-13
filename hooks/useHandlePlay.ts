6b7fa4f166ecd8bfb44e15971119092cc970681b
RedRe4per <s3582474@gmail.com>
2023-03-14 01:31:25 +1100

    feat: abort prev fetching

--- hooks/useHandlePlay.ts
+++ hooks/useHandlePlay.ts
@@ -1,14 +1,16 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { PlayerContext } from "@/contexts/PlayerContext";
 
 export const useHandlePlay = (albumId: number) => {
   const { setPlayerList, setAlbum } = useContext(PlayerContext);
+  const controller = new AbortController();
+  const signal = controller.signal;
 
   const handlePlay = async (e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/album?id=${albumId}`,
-      { credentials: "include" }
+      { signal: signal }
     );
     const albumData = await response.json();
     setAlbum(albumData.album);
@@ -17,7 +19,7 @@ export const useHandlePlay = (albumId: number) => {
     if (!albumData.songs[0].id) return;
     const firstSongResponse = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/song/url/v1?id=${albumData.songs[0].id}&level=higher`,
-      { credentials: "include" }
+      { signal: signal }
     );
     const firstSongData = await firstSongResponse.json();
     if (!firstSongData.data[0].url) console.log("no url here!"); //process later
@@ -31,7 +33,7 @@ export const useHandlePlay = (albumId: number) => {
       `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/song/url/v1?id=${songList.join(
         ","
       )}&level=higher`,
-      { credentials: "include" }
+      { signal: signal }
     );
     const songsData = await songsResponse.json();
     const sortedList = songsData.data.sort((a: any, b: any) => {
