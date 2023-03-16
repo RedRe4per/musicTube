bd4fd15511fcb66e1e207bca43b1027d0209a8f0
RedRe4per <s3582474@gmail.com>
2023-03-16 13:17:06 +1100

    fix: zlib bug fix test

--- next.config.js
+++ next.config.js
@@ -2,16 +2,15 @@
 if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
   )
 ) {
   process.env.LD_LIBRARY_PATH = `${
     process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
 }
 
 const nextConfig = {
-  
   reactStrictMode: true,
   images: {
     domains: [
