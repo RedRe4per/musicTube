9e1d3813a7babcf5dfa1599cf54e2d6331bf33f1
RedRe4per <s3582474@gmail.com>
2023-03-16 17:34:54 +1100

    feat: album play

--- components/AlbumPlay/index.tsx
+++ components/AlbumPlay/index.tsx
@@ -2,36 +2,36 @@ import Image from "next/image";
 import { useHandlePlay } from "@/hooks/useHandlePlay";
 
 interface Props {
-    albumId: number;
+  albumId: number;
 }
 
-export const AlbumPlay = ({albumId}: Props) => {
-    const { handlePlay } = useHandlePlay(albumId);
+export const AlbumPlay = ({ albumId }: Props) => {
+  const { handlePlay } = useHandlePlay(albumId);
 
-    return (
-        <section className="mt-6 ml-10 flex gap-10">
-        <button onClick={handlePlay} className="hover:animate-pulse">
-          <Image
-            src="/icons/play-circle-fill.svg"
-            alt="play"
-            width={100}
-            height={100}
+  return (
+    <section className="mt-6 ml-10 flex gap-10">
+      <button onClick={handlePlay} className="hover:animate-pulse">
+        <Image
+          src="/icons/play-circle-fill.svg"
+          alt="play"
+          width={100}
+          height={100}
+        />
+      </button>
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="52"
+          height="52"
+        >
+          <path fill="none" d="M0 0H24V24H0z" />
+          <path
+            d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z"
+            fill="#1DB954"
           />
-        </button>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="52"
-            height="52"
-          >
-            <path fill="none" d="M0 0H24V24H0z" />
-            <path
-              d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z"
-              fill="#1DB954"
-            />
-          </svg>
-        </button>
-      </section>
-    )
-}
+        </svg>
+      </button>
+    </section>
+  );
+};
