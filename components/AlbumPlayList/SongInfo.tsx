f4eb839108de5fdebe756feccdeada65560122ff
RedRe4per <s3582474@gmail.com>
2023-03-16 20:41:29 +1100

    feat: mix color

--- components/AlbumPlayList/SongInfo.tsx
+++ components/AlbumPlayList/SongInfo.tsx
@@ -15,9 +15,7 @@ export const SongInfo = React.memo(({ song, index }: Props) => {
     setIndexDisplay(mode);
   };
 
-  const handlePlay = () => {
-
-  };
+  const handlePlay = () => {};
 
   return (
     <section
@@ -27,7 +25,10 @@ export const SongInfo = React.memo(({ song, index }: Props) => {
       className="flex py-3 pl-3 pr-10 items-center hover:bg-gray-400 rounded-lg"
     >
       <div className="flex-1 flex items-center">
-        <section onClick={handlePlay} className="w-16 flex justify-center items-center">
+        <section
+          onClick={handlePlay}
+          className="w-16 flex justify-center items-center"
+        >
           <h6
             className="text-gray-200"
             style={{ display: indexDisplay === "play" ? "none" : "" }}
--- utils/mixColor.ts
+++ utils/mixColor.ts
@@ -1,31 +1,30 @@
 type RGBColor = {
-    r: number;
-    g: number;
-    b: number;
-  };
-  
+  r: number;
+  g: number;
+  b: number;
+};
+
 export const mixColors = (color1: string, color2: string): string => {
-    const rgbToObj = (rgbString: string): RGBColor => {
-      const rgbValues = rgbString
-        .substring(rgbString.indexOf("(") + 1, rgbString.lastIndexOf(")"))
-        .split(",")
-        .map((value) => parseInt(value.trim(), 10));
-  
-      return { r: rgbValues[0], g: rgbValues[1], b: rgbValues[2] };
-    };
-  
-    const objToRgb = (color: RGBColor): string => {
-      const { r, g, b } = color;
-      return `rgb(${r}, ${g}, ${b})`;
-    };
-  
-    const { r: r1, g: g1, b: b1 } = rgbToObj(color1);
-    const { r: r2, g: g2, b: b2 } = rgbToObj(color2);
-  
-    const r = Math.round((r1 + r2) / 2);
-    const g = Math.round((g1 + g2) / 2);
-    const b = Math.round((b1 + b2) / 2);
-  
-    return objToRgb({ r, g, b });
-  }
-  
+  const rgbToObj = (rgbString: string): RGBColor => {
+    const rgbValues = rgbString
+      .substring(rgbString.indexOf("(") + 1, rgbString.lastIndexOf(")"))
+      .split(",")
+      .map((value) => parseInt(value.trim(), 10));
+
+    return { r: rgbValues[0], g: rgbValues[1], b: rgbValues[2] };
+  };
+
+  const objToRgb = (color: RGBColor): string => {
+    const { r, g, b } = color;
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  const { r: r1, g: g1, b: b1 } = rgbToObj(color1);
+  const { r: r2, g: g2, b: b2 } = rgbToObj(color2);
+
+  const r = Math.round((r1 + r2) / 2);
+  const g = Math.round((g1 + g2) / 2);
+  const b = Math.round((b1 + b2) / 2);
+
+  return objToRgb({ r, g, b });
+};
