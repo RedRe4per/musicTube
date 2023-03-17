0cbd4b4129074d9b5a30381844f6832a8f7ce31a
RedRe4per <s3582474@gmail.com>
2023-03-17 23:16:33 +1100

    feat: playlist page init and interface

--- interfaces/music.d.ts
+++ interfaces/music.d.ts
@@ -38,3 +38,72 @@ export interface IMusicDetail {
   effectTypes: null;
   time: number;
 }
+
+export interface ITrack {
+  name: string;
+  id: number;
+  pst: number;
+  t: number;
+  ar: [{
+    id: number;
+    name: string;
+  }];
+  pop: number;
+  st: number
+  rt: string;
+  fee: number;
+  v: number;
+  cf: string;
+  al: {
+    id: number;
+    name: string
+    picUrl: string;
+    pic: number;
+  }
+  dt: number;
+  h: {
+    br: number;
+    fid: number;
+    size: number,
+    vd: number;
+    sr: number;
+  };
+  m: {
+    br: number;
+    fid: number;
+    size: number,
+    vd: number;
+    sr: number;
+  };
+  l: {
+    br: number;
+    fid: number;
+    size: number,
+    vd: number;
+    sr: number;
+  };
+  cd: string;
+  no: number;
+  ftype: number;
+  djId: number;
+  copyright: number;
+  s_id: number;
+  mark: number;
+  originCoverType: number;
+  version: number;
+  single: number;
+  rtype: number;
+  mst: number;
+  cp: number;
+  mv: number;
+  publishTime: number;
+}
+
+export interface ITrackId {
+  id: number;
+  v: number;
+  t: number;
+  at: number;
+  uid: number;
+  rcmdReason: string;
+}
---
+++ pages/playlist/[id].tsx
@@ -0,0 +1,60 @@
+import { GetServerSidePropsContext } from "next";
+import { useBackgroundColor } from "@/hooks/useBackgroundColor";
+import { BgColorContext } from "@/contexts/BgColorContext";
+import { useContext } from "react";
+import { IAlbumDetails } from "@/interfaces/album";
+import { IAlbumSong } from "@/interfaces/albumSong";
+import { AlbumInfo } from "@/components/AlbumInfo";
+import { AlbumPlay } from "@/components/AlbumPlay";
+import { AlbumPlayList } from "@/components/AlbumPlayList";
+import { mixColor } from "@/utils/mixColor";
+import { Footer } from "@/layouts/footer";
+
+interface Props {
+  album: IAlbumDetails;
+  code: number;
+  resourceState: boolean;
+  songs: IAlbumSong[];
+}
+
+export default function Album(albumInfo: Props) {
+  const { type, name, picUrl, description, subType, artists, id } =
+    albumInfo.album;
+  const { handleBackgroundColor } = useBackgroundColor(picUrl);
+  const { bgColor } = useContext(BgColorContext);
+  if (typeof window !== "undefined") {
+    handleBackgroundColor();
+  }
+
+  return (
+    <main
+      className="transition-transform duration-1000 shadow-inner shadow-gray-650"
+      style={{
+        background: `linear-gradient(to bottom, #1B1B1B, ${mixColor(
+          "#1B1B1B",
+          bgColor
+        )})`,
+      }}
+    >
+      <AlbumInfo
+        picUrl={picUrl}
+        type={type}
+        subType={subType}
+        name={name}
+        artists={artists}
+      />
+      <AlbumPlay albumId={id} />
+      <AlbumPlayList albumSongs={albumInfo.songs} albumId={id} />
+      <Footer />
+    </main>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { id } = context.query;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/album?id=${id}`
+  );
+  const albumInfo = await response.json();
+  return { props: albumInfo };
+}
