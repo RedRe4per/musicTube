d8635082830dde41eb3658272b14ccf0755b71b7
RedRe4per <s3582474@gmail.com>
2023-03-17 17:09:43 +1100

    feat: album page song pause button display

--- components/AlbumPlayList/SongInfo.tsx
+++ components/AlbumPlayList/SongInfo.tsx
@@ -45,9 +45,7 @@ export const SongInfo = React.memo(({ song, index, albumId }: Props) => {
       className="flex py-3 pl-3 pr-10 items-center hover:bg-gray-400 rounded-lg"
     >
       <div className="flex-1 flex items-center">
-        <section
-          className="w-16 flex justify-center items-center"
-        >
+        <section className="w-16 flex justify-center items-center">
           <h6
             className={`text-gray-200 ${
               indexDisplay === "play" ? "hidden" : ""
@@ -55,27 +53,60 @@ export const SongInfo = React.memo(({ song, index, albumId }: Props) => {
           >
             {index}
           </h6>
-          <div onClick={handlePlayClick} className={indexDisplay === "index" || (!isMusicPlay && id === currentMusic?.id) ? "hidden" : ""}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="32"
-            height="32"
+          <div
+            onClick={handlePlayClick}
+            className={
+              indexDisplay === "index" ||
+              (!isMusicPlay && id === currentMusic?.id)
+                ? "hidden"
+                : ""
+            }
           >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path
-              d="M7.752 5.439l10.508 6.13a.5.5 0 0 1 0 .863l-10.508 6.13A.5.5 0 0 1 7 18.128V5.871a.5.5 0 0 1 .752-.432z"
-              fill="#ffffff"
-            />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="32"
+              height="32"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path
+                d="M7.752 5.439l10.508 6.13a.5.5 0 0 1 0 .863l-10.508 6.13A.5.5 0 0 1 7 18.128V5.871a.5.5 0 0 1 .752-.432z"
+                fill="#ffffff"
+              />
+            </svg>
           </div>
-          <div onClick={handlePauseClick} className={indexDisplay === "play" && !isMusicPlay && id === currentMusic?.id ? "" : "hidden"}>
-          <svg 
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path fill="none" d="M0 0h24v24H0z"/><path d="M15 7a1 1 0 0 1 2 0v10a1 1 0 1 1-2 0V7zM7 7a1 1 0 1 1 2 0v10a1 1 0 1 1-2 0V7z" fill="#ffffff"/></svg>
+          <div
+            onClick={handlePauseClick}
+            className={
+              indexDisplay === "play" && !isMusicPlay && id === currentMusic?.id
+                ? ""
+                : "hidden"
+            }
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="32"
+              height="32"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path
+                d="M15 7a1 1 0 0 1 2 0v10a1 1 0 1 1-2 0V7zM7 7a1 1 0 1 1 2 0v10a1 1 0 1 1-2 0V7z"
+                fill="#ffffff"
+              />
+            </svg>
           </div>
         </section>
         <div className="flex flex-col gap-2">
-          <div className={id === currentMusic?.id ? "text-green brightness-150": "text-white-200"}>{name}</div>
+          <div
+            className={
+              id === currentMusic?.id
+                ? "text-green brightness-150"
+                : "text-white-200"
+            }
+          >
+            {name}
+          </div>
           <div className="text-gray-200 text-tag-light">
             {ar.map((artist, index) => {
               return <span key={index}>{artist.name}&nbsp;&nbsp;</span>;
