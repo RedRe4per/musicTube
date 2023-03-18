8c56b3b11df941315cfcf801cbf711c823c0b2f0
RedRe4per <s3582474@gmail.com>
2023-03-18 23:01:04 +1100

    feat: search result no results display

--- components/SearchBar/SearchResult.tsx
+++ components/SearchBar/SearchResult.tsx
@@ -10,9 +10,12 @@ export const SearchResult = React.memo(({ searchResult }: Props) => {
     <section
       className={`${
         searchResult ? "" : "hidden"
-      } absolute w-[650px] h-[500px] bg-gray-800 border-2 border-solid border-gray-400 shadow-xl shadow-gray-400 rounded-2xl brightness-150 top-16 left-1/2 transform -translate-x-1/2 z-10`}
+      } absolute w-[650px] bg-gray-800 border-2 border-solid border-gray-400 shadow-xl shadow-gray-400 rounded-2xl brightness-150 top-16 left-1/2 transform -translate-x-1/2 z-10`}
     >
-      <div>121231231231233</div>
+      <section className="p-5">
+        {!searchResult?.songs && <h5 className="italic text-h4-light text-gray-200">No results found</h5>}
+
+      </section>
     </section>
   );
 });
