4b768650524612ac5eab2dd657e8c611f9c7d319
RedRe4per <s3582474@gmail.com>
2023-03-18 18:51:43 +1100

    feat: show all link in main page

--- components/Playlist/PlaylistList.tsx
+++ components/Playlist/PlaylistList.tsx
@@ -11,8 +11,10 @@ export const PlaylistList = ({ title, playlistList }: Props) => {
   return (
     <section className="ml-5 mt-6 overflow-hidden">
       <section className="flex items-center justify-between">
-      <h2 className="ml-1 text-h3-normal text-white-200">{title}</h2>
-      <Link className="text-h4-normal mr-4" href={"/"}>Show all</Link>
+        <h2 className="ml-1 text-h3-normal text-white-200">{title}</h2>
+        <Link className="text-h4-normal mr-4" href={"/"}>
+          Show all
+        </Link>
       </section>
       <section className="mt-2 flex gap-4 w-[350vw] sm:w-[310vw] md:w-[270vw] lg:w-[220vw] xl:w-[170vw] 2xl:w-[130vw] custom:w-[100vw]">
         {playlistList.playlists.map((playlist: IPlaylist) => {
