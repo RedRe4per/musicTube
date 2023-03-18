7d0c799959c0bfa1cdd5ba9684c1b0ba763686a2
RedRe4per <s3582474@gmail.com>
2023-03-18 23:03:49 +1100

    fix: fix no result found condition

--- components/SearchBar/SearchResult.tsx
+++ components/SearchBar/SearchResult.tsx
@@ -13,7 +13,7 @@ export const SearchResult = React.memo(({ searchResult }: Props) => {
       } absolute w-[650px] bg-gray-800 border-2 border-solid border-gray-400 shadow-xl shadow-gray-400 rounded-2xl brightness-150 top-16 left-1/2 transform -translate-x-1/2 z-10`}
     >
       <section className="p-5">
-        {!searchResult?.songs && (
+        {!searchResult?.result && (
           <h5 className="italic text-h4-light text-gray-200">
             No results found
           </h5>
