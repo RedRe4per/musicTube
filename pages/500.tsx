a7bb22e9c6bcbcb0929c45f3a14b0128c86f294d
RedRe4per <s3582474@gmail.com>
2023-03-18 16:52:03 +1100

    feat: 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Custom500() {
+    return (
+        <section className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <Image src="/images/error500.jpg" alt="500Error" className="max-w-sm rounded-lg shadow-2xl" width={200} height={400} />
+    <div>
+      <h1 className="text-5xl font-bold">No resource found!</h1>
+      <p className="py-6">Sorry, it seems that the resource you need is not available in the server, maybe your resource Id is wrong, or other reasons. Please check and request again.</p>
+      <Link href={"/"}>
+      <button className="btn btn-primary">Home Page</button>
+      </Link>
+    </div>
+  </div>
+</section>
+    )
+  }
