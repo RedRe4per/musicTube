292f3e1cd0a96af96168e7ef1253e2186cb7db98
RedRe4per <s3582474@gmail.com>
2023-03-18 22:10:40 +1100

    feat: search debounce

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,14 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { debounce } from "lodash";
+
 interface Props {
   page: string;
 }
 
 export const SearchBar = ({ page }: Props) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  useEffect(() => {
+    const debouncedSearch = debounce((term) => {
+      setDebouncedSearchTerm(term);
+    }, 500);
+    debouncedSearch(searchTerm);
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      const searchRequest = async() => {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/cloudsearch?keywords=${debouncedSearchTerm}`);
+        const data = await response.json();
+        console.log(data, "result")
+      }
+      searchRequest();
+    }
+  }, [debouncedSearchTerm]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <section>
       <input
         className={`flex bg-secondary w-[300px] xl:w-[450px] 2xl:w-[600px] h-[50px] pl-[54px] pt-1.5 rounded-[10px] placeholder:text-h4-light placeholder:text-gray-200 ${
           page === "/likedSongs" ? "invert" : ""
         }`}
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
         placeholder="Search"
         autoFocus
       />
