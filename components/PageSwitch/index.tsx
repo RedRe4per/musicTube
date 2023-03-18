c23dff76ff3978b198e7252238c31fe1148fb7e7
RedRe4per <s3582474@gmail.com>
2023-03-18 21:11:05 +1100

    feat: go forward and go back button logic

--- components/PageSwitch/index.tsx
+++ components/PageSwitch/index.tsx
@@ -1,18 +1,34 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
 interface Props {
   page: string;
 }
 
 export const PageSwitch = ({ page }: Props) => {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  const handleGoForward = () => {
+    const { history } = window;
+    if (history.length > 1) {
+      history.forward();
+    }
+  };
+
   return (
     <section
       className={`hidden lg:flex bg-secondary h-[50px] rounded-[10px] ${
         page === "/likedSongs" ? "invert" : ""
       }`}
     >
-      <button className="w-[60px]">
+      <button onClick={handleGoBack} className="w-[60px]">
         <img src="/icons/arrow-left.svg" alt="last-page" className="m-auto" />
       </button>
-      <button className="w-[60px]">
+      <button onClick={handleGoForward} className="w-[60px]">
         <img
           src="/icons/arrow-right.svg"
           alt="next-page"
