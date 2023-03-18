fa585f958b90daee7c5340ffc8784a0513a2e2cd
RedRe4per <s3582474@gmail.com>
2023-03-18 22:55:04 +1100

    feat: search result style

--- components/SearchBar/SearchResult.tsx
+++ components/SearchBar/SearchResult.tsx
@@ -1,20 +1,18 @@
+import React from "react";
+
 interface Props {
   searchResult: any;
 }
 
-export const SearchResult = ({ searchResult }: Props) => {
+export const SearchResult = React.memo(({ searchResult }: Props) => {
   console.log(searchResult);
   return (
     <section
       className={`${
         searchResult ? "" : "hidden"
-      } absolute brightness-150 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10`}
+      } absolute w-[650px] h-[500px] bg-gray-800 border-2 border-solid border-gray-400 shadow-xl shadow-gray-400 rounded-2xl brightness-150 top-16 left-1/2 transform -translate-x-1/2 z-10`}
     >
-      <div>121231231231233</div>
-      <div>121231231231233</div>
-      <div>121231231231233</div>
-      <div>121231231231233</div>
       <div>121231231231233</div>
     </section>
   );
-};
+});
