0dd23cf7b085a547bd372a63854017153a7539c8
RedRe4per <s3582474@gmail.com>
2023-03-19 15:33:10 +1100

    feat: album and playlist search logic

--- components/SearchBar/SearchResult.tsx
+++ components/SearchBar/SearchResult.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 export const SearchResult = React.memo(({ searchResult }: Props) => {
-  console.log(searchResult)
+  console.log(searchResult);
   return (
     <section
       className={`${
@@ -16,7 +16,11 @@ export const SearchResult = React.memo(({ searchResult }: Props) => {
       } absolute min-w-[600px] max-w-[700px] max-h-[70vh] overflow-x-hidden overflow-y-scroll scrollbar bg-gray-800 border-2 border-solid border-gray-400 shadow-lg shadow-gray-400 rounded-lg brightness-150 top-16 left-1/2 transform -translate-x-1/2 z-10`}
     >
       <section className="px-5 py-3 flex flex-col">
-        {(!searchResult?.result || !searchResult?.result.songs || !searchResult?.result.albums || !searchResult?.result.playlists || !searchResult?.result.artists) && (
+        {(!searchResult?.result ||
+          !searchResult?.result.songs ||
+          !searchResult?.result.albums ||
+          !searchResult?.result.playlists ||
+          !searchResult?.result.artists) && (
           <h5 className="italic text-h4-light text-gray-200">
             No results found
           </h5>
