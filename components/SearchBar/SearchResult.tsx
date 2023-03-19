58905e5f1a58c3c21007003071921320d23a6485
RedRe4per <s3582474@gmail.com>
2023-03-19 17:50:04 +1100

    feat: combine no result determine logic

--- components/SearchBar/SearchResult.tsx
+++ components/SearchBar/SearchResult.tsx
@@ -15,7 +15,7 @@ const COMPONENTS_MAP: ComponentMap = {
   artist: ArtistItem,
 };
 
-const keysToCheck = ['songs', 'albums', 'playlists', 'artists'];
+const keysToCheck = ["songs", "albums", "playlists", "artists"];
 
 interface Props {
   searchResult: any;
@@ -29,8 +29,10 @@ export const SearchResult = React.memo(({ searchResult }: Props) => {
       } absolute min-w-[600px] max-w-[700px] max-h-[70vh] overflow-x-hidden overflow-y-scroll scrollbar bg-gray-800 border-2 border-solid border-gray-400 shadow-lg shadow-gray-400 rounded-lg brightness-150 top-16 left-1/2 transform -translate-x-1/2 z-10`}
     >
       <section className="px-5 py-3 flex flex-col">
-        {(!searchResult?.result || !Object.keys(searchResult.result).some(key => keysToCheck.includes(key)))
-           && (
+        {(!searchResult?.result ||
+          !Object.keys(searchResult.result).some((key) =>
+            keysToCheck.includes(key)
+          )) && (
           <h5 className="italic text-h4-light text-gray-200">
             No results found
           </h5>
