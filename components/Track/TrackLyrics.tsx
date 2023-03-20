1b92878b878761dab0ddd80b05d95b323a4caf89
RedRe4per <s3582474@gmail.com>
2023-03-20 18:50:02 +1100

    feat: track lyrics

--- components/Track/TrackLyrics.tsx
+++ components/Track/TrackLyrics.tsx
@@ -2,33 +2,40 @@ import { useEffect, useState } from "react";
 import { parseLyrics } from "@/utils/parseLyrics";
 
 interface Props {
-    trackId: number;
+  trackId: number;
 }
 
 interface FormattedLyrics {
-   time: number;
-   text: string; 
+  time: number;
+  text: string;
 }
 
 export const TrackLyrics = ({ trackId }: Props) => {
-    const [lyrics, setLyrics] = useState<FormattedLyrics[] | null>(null);
-    useEffect(()=>{
-        const getLyrics = async () => {
-            const lyricResponse = await fetch(
-                `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/lyric?id=${trackId}`
-              );
-              const lyricData = await lyricResponse.json();
-              const formattedLyrics = parseLyrics(lyricData.lrc.lyric) as FormattedLyrics[];
-              setLyrics(formattedLyrics)   
-        }
-        getLyrics();
-    }, [trackId])
+  const [lyrics, setLyrics] = useState<FormattedLyrics[] | null>(null);
+  useEffect(() => {
+    const getLyrics = async () => {
+      const lyricResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/lyric?id=${trackId}`
+      );
+      const lyricData = await lyricResponse.json();
+      const formattedLyrics = parseLyrics(
+        lyricData.lrc.lyric
+      ) as FormattedLyrics[];
+      setLyrics(formattedLyrics);
+    };
+    getLyrics();
+  }, [trackId]);
 
-    return (
-        <section className="ml-10 mt-10 text-h4-light">
-            {lyrics && lyrics.map((lyric) => {
-              return <p className="mt-[10px]" key={lyric.time}>{lyric.text}</p>;
-            })}
-        </section>
-    )
-}
+  return (
+    <section className="ml-10 mt-10 text-h4-light">
+      {lyrics &&
+        lyrics.map((lyric) => {
+          return (
+            <p className="mt-[10px]" key={lyric.time}>
+              {lyric.text}
+            </p>
+          );
+        })}
+    </section>
+  );
+};
--- utils/parseLyrics.ts
+++ utils/parseLyrics.ts
@@ -1,17 +1,19 @@
 export const parseLyrics = (rawData: string) => {
-    const lines = rawData.split('\n');
-    const parsedData = lines.map(line => {
+  const lines = rawData.split("\n");
+  const parsedData = lines
+    .map((line) => {
       const timestampMatch = line.match(/\[(\d{2}):(\d{2})\.(\d{2,3})\]/);
       if (timestampMatch) {
         const minutes = parseInt(timestampMatch[1], 10);
         const seconds = parseInt(timestampMatch[2], 10);
         const milliseconds = parseInt(timestampMatch[3], 10);
         const time = minutes * 60 * 1000 + seconds * 1000 + milliseconds;
-        const text = line.replace(timestampMatch[0], '').trim();
+        const text = line.replace(timestampMatch[0], "").trim();
         return { time, text };
       }
       return null;
-    }).filter(item => item !== null);
-  
-    return parsedData;
-  }
+    })
+    .filter((item) => item !== null);
+
+  return parsedData;
+};
