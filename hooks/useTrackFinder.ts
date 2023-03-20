a570bc6d52c8c996844fde76edad98092fb29593
RedRe4per <s3582474@gmail.com>
2023-03-21 03:04:44 +1100

    feat: add use track finder hooks to fix track play

--- hooks/useTrackFinder.ts
+++ hooks/useTrackFinder.ts
@@ -2,29 +2,36 @@ import { useEffect, useContext } from "react";
 import { AlertContext } from "@/contexts/AlertContext";
 import { IAlbumSong } from "@/interfaces/albumSong";
 
-export const useTrackFinder = (albumId: number, trackId: number, setSongIndex: (newValue: number | ((prevValue: number) => number)) => void, setPlayDisabled: (newValue: boolean | ((prevValue: boolean) => boolean)) => void) => {
-    const { setAlertBox } = useContext(AlertContext);
+export const useTrackFinder = (
+  albumId: number,
+  trackId: number,
+  setSongIndex: (newValue: number | ((prevValue: number) => number)) => void,
+  setPlayDisabled: (
+    newValue: boolean | ((prevValue: boolean) => boolean)
+  ) => void
+) => {
+  const { setAlertBox } = useContext(AlertContext);
 
-    useEffect(() => {
-        const getTrackIndex = async () => {
-          if (albumId === 0) {
-            setAlertBox({ message: "No song in this Album!" });
-            return;
-          }
-          const response = await fetch(
-            `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/album?id=${albumId}`
-          );
-          const albumData = await response.json();
-          if (!albumData.songs) {
-            setAlertBox({ message: "No song in this Album!" });
-            return;
-          }
-          const index = albumData.songs.findIndex(
-            (song: IAlbumSong) => song.id === trackId
-          );
-          setSongIndex(index);
-          setPlayDisabled(false);
-        };
-        getTrackIndex();
-      }, [trackId]);
-}
+  useEffect(() => {
+    const getTrackIndex = async () => {
+      if (albumId === 0) {
+        setAlertBox({ message: "No song in this Album!" });
+        return;
+      }
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_ADDRESS}/album?id=${albumId}`
+      );
+      const albumData = await response.json();
+      if (!albumData.songs) {
+        setAlertBox({ message: "No song in this Album!" });
+        return;
+      }
+      const index = albumData.songs.findIndex(
+        (song: IAlbumSong) => song.id === trackId
+      );
+      setSongIndex(index);
+      setPlayDisabled(false);
+    };
+    getTrackIndex();
+  }, [trackId]);
+};
