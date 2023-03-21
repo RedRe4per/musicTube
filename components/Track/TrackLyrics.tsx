fa031e531fcf48302bd0a9f3dd0f3061d0284378
RedRe4per <s3582474@gmail.com>
2023-03-21 15:57:30 +1100

    fix: optimize timer

--- components/Track/TrackLyrics.tsx
+++ components/Track/TrackLyrics.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState, useContext } from "react";
+import { useRouter } from "next/router";
 import { LyricsContext } from "@/contexts/LyricsContext";
 import { PlayAndPauseContext } from "@/contexts/PlayAndPauseContext";
 import { parseLyrics } from "@/utils/parseLyrics";
@@ -14,26 +15,26 @@ interface FormattedLyrics {
 }
 
 export const TrackLyrics = ({ trackId }: Props) => {
+  const router = useRouter();
+  const pathId = +router.asPath.split("/")[2];
   const { musicPlayer } = useContext(LyricsContext);
   const [lyricsTimestamp, setLyricsTimestamp] = useState(0);
   const { isMusicPlay, currentMusic } = useContext(PlayAndPauseContext);
-  const [intervalId, setIntervalId] = useState<number | NodeJS.Timeout>(0);
   const [lyrics, setLyrics] = useState<FormattedLyrics[] | null>(null);
+  let intervalId: number | NodeJS.Timeout = 0;
 
   useEffect(() => {
     if (!isMusicPlay && currentMusic?.id === trackId) {
-      setIntervalId(
-        setInterval(() => {
+      intervalId = setInterval(() => {
           setLyricsTimestamp(musicPlayer?.current.currentTime);
         }, 200)
-      );
     } else {
       clearInterval(intervalId);
     }
     return () => {
       clearInterval(intervalId);
     };
-  }, [isMusicPlay, trackId]);
+  }, [isMusicPlay, currentMusic?.id, pathId]);
 
   useEffect(() => {
     const getLyrics = async () => {
@@ -55,13 +56,12 @@ export const TrackLyrics = ({ trackId }: Props) => {
         lyrics.map((lyric, index) => {
           return (
             <p
-              className={`mt-[10px] ${
-                currentMusic?.id === trackId &&
-                lyricsTimestamp * 1000 > lyric.startTime &&
-                lyricsTimestamp * 1000 < lyric.endTime
+              className={`mt-[10px] ${currentMusic?.id === trackId &&
+                  lyricsTimestamp * 1000 > lyric.startTime &&
+                  lyricsTimestamp * 1000 < lyric.endTime
                   ? "text-green brightness-150 text-h4-normal"
                   : ""
-              }`}
+                }`}
               key={index}
             >
               {lyric.text}
