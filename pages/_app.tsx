1718a8cd2d13a8a2d545803e6963b728f68a4470
RedRe4per <s3582474@gmail.com>
2023-03-23 15:57:18 +1100

    fix: loading fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Rubik } from "next/font/google";
 import Layout from "@/components/layout";
-import { useRouter } from "next/router";
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import { IMusicDetail } from "@/interfaces/music";
 import { IAlertBox } from "@/interfaces/alertBox";
 import { PlayerContext } from "@/contexts/PlayerContext";
@@ -20,8 +19,6 @@ const rubik = Rubik({
 
 export default function App({ Component, pageProps }: AppProps) {
   const musicPlayer = useRef<HTMLAudioElement>(null);
-  const router = useRouter();
-  const [hasError, setHasError] = useState(false);
   const [playerList, setPlayerList] = useState<IMusicDetail[]>([]);
   const [bgColor, setBgColor] = useState("gray-650");
   const [isLoading, setIsLoading] = useState(false);
@@ -33,36 +30,6 @@ export default function App({ Component, pageProps }: AppProps) {
     message: "",
     messageType: "alert-warning",
   });
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      if (hasError) {
-        setHasError(false);
-      }
-    };
-
-    router.events.on("routeChangeStart", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChange);
-    };
-  }, [router.events, hasError]);
-
-  useEffect(() => {
-    const handleRouteError = (err: any) => {
-      if (err.message === "Loading initial props cancelled") {
-        console.log("SSR request was cancelled due to client-side navigation.");
-        setHasError(true);
-      } else {
-        // 对于其他类型的错误，你可以将它们发送到日志系统或执行其他错误处理操作
-        console.error("Unhandled error:", err);
-      }
-    };
-
-    router.events.on("routeChangeError", handleRouteError);
-    return () => {
-      router.events.off("routeChangeError", handleRouteError);
-    };
-  }, [router.events]);
 
   return (
     <main className={`${rubik.variable} font-sans`}>
