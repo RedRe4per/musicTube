9a3afaf41673853f7168973ade255a2f5c871e6d
RedRe4per <s3582474@gmail.com>
2023-03-23 16:24:25 +1100

    feat: error handler test

--- pages/_error.js
+++ pages/_error.js
@@ -1,20 +1,20 @@
 // @ts-nocheck
 // _error.tsx
 function Error({ statusCode }) {
-  console.log("123")
+  console.log("123");
   return (
     <p>
       {statusCode
         ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
+        : "An error occurred on client"}
     </p>
-  )
+  );
 }
 
 Error.getInitialProps = ({ res, err }) => {
-  console.log("456")
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
+  console.log("456");
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
 
-export default Error
+export default Error;
