4cd2287a465f3803350e1dabcb0311495c83957e
RedRe4per <s3582474@gmail.com>
2023-03-26 22:47:55 +1100

    feat: playlist page pagination init

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,11 +1,13 @@
 import { GetServerSidePropsContext } from "next";
 import { IPlaylist, IPrivilege } from "@/interfaces/playlist";
+import { Track } from "@/interfaces/playlist";
 import { useBackgroundColor } from "@/hooks/useBackgroundColor";
 import { BgColorContext } from "@/contexts/BgColorContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PlaylistInfo } from "@/components/Playlist/PlaylistInfo";
 import { PlaylistPlay } from "@/components/Playlist/PlaylistPlay";
 import { SongList } from "@/components/Playlist/SongList";
+import { PlaylistPagination } from "@/components/Playlist/PlaylistPagination";
 import { mixColor } from "@/utils/mixColor";
 import { Footer } from "@/layouts/footer";
 
@@ -30,11 +32,15 @@ export default function Playlist(playlistInfo: Props) {
     subscribedCount,
     tracks,
   } = playlistInfo.playlist;
+  console.log(tracks)
   const { handleBackgroundColor } = useBackgroundColor(coverImgUrl);
   const { bgColor, setIsLoading } = useContext(BgColorContext);
+  const [ currentTracks, setCurrentTracks ] = useState<Track[]>([]);
+  const [ currentPage, setCurrentPage ] = useState(1);
 
   useEffect(() => {
     setIsLoading(false);
+    setCurrentTracks(tracks);
   }, [id]);
 
   if (typeof window !== "undefined") {
@@ -62,7 +68,8 @@ export default function Playlist(playlistInfo: Props) {
         subscribedCount={subscribedCount}
       />
       <PlaylistPlay playlistId={id} trackCount={trackCount} />
-      <SongList playlistSongs={tracks} playlistId={id} />
+      <SongList playlistSongs={currentTracks} playlistId={id} />
+      <PlaylistPagination itemQty={trackCount} currentPage={currentPage}/>
       <Footer />
     </main>
   );
