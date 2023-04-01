01138e4b7b166875cc95c5c4ceeebc16a005db4f
RedRe4per <s3582474@gmail.com>
2023-04-02 02:23:57 +1100

    feat: browse page init

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -1,18 +1,75 @@
 import { useContext, useEffect } from "react";
 import { BgColorContext } from "@/contexts/BgColorContext";
+import { switchTopPlaylistTag } from "@/utils/switchTopPlaylistTag";
+import { mixColor } from "@/utils/mixColor";
 
-export default function Home() {
+interface CatListItem {
+  category: number;
+  hot: boolean;
+  name: string;
+  resourceCount: number;
+  type: number;
+}
+
+export default function Browse(allPlaylistTag: any) {
   const { setIsLoading } = useContext(BgColorContext);
 
   useEffect(() => {
     setIsLoading(false);
   }, []);
 
-  return (
+  console.log(allPlaylistTag)
+
+return (
     <>
       <main>
-        <section className="">browse page</section>
+        <section>browse page</section>
       </main>
     </>
   );
 }
+
+  
+  export async function getStaticProps() {
+    const catListRes = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_SERVER_ADDRESS
+      }/playlist/catlist`
+    );
+    const catListData = await catListRes.json();
+    const catList = catListData.sub;
+
+    async function fetchData(keyword: CatListItem) {
+      const response = await fetch(
+        `${
+          process.env.NEXT_PUBLIC_SERVER_ADDRESS
+        }/cloudsearch?type=1000&keywords=${keyword.name}&timestamp=${Date.now()}`
+      );
+      const playlistData = await response.json();
+      const playlistImage = playlistData.result.playlists[0].coverImgUrl;
+
+      const colorRes = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/colorExtract?imageUrl=${playlistImage}`);
+      const color = await colorRes.json();
+
+      return {playlist: switchTopPlaylistTag(keyword.name), color: mixColor("#1B1B1B", color.dominantColor), imageUrl: playlistImage }
+    }
+
+    async function fetchAllData(catList: CatListItem[]) {
+      try {
+        const promises = catList.map((keyword: CatListItem) => fetchData(keyword));
+        const results = await Promise.all(promises);
+        return results;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+      }
+    }
+
+    const allPlaylistTag = await fetchAllData(catList).then(data => {
+      return data;
+    }).catch(error => {
+      console.error("Error in fetchAllData:", error);
+    });
+
+    return { props: {allPlaylistTag} };
+  }
---
+++ pages/genre/[id].tsx
@@ -0,0 +1,20 @@
+import { GetServerSidePropsContext } from "next";
+
+export default function Genre(artistInfo: any) {
+    
+    return (
+      <main>
+      </main>
+    );
+  }
+  
+  export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const { id } = context.query;
+    const response = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_SERVER_ADDRESS
+      }/artists?id=${id}&timestamp=${Date.now()}`
+    );
+    const artistInfo = await response.json();
+    return { props: artistInfo };
+  }
