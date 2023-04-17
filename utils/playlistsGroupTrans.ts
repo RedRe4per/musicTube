c8a06a50aca0331c48598d0636f4a137f932c053
RedRe4per <s3582474@gmail.com>
2023-04-17 23:53:52 +1000

    fix: translator

--- utils/playlistsGroupTrans.ts
+++ utils/playlistsGroupTrans.ts
@@ -3,25 +3,25 @@ import { IPlaylistList, IPlaylist } from "@/interfaces/playlist";
 export const playlistsGroupTranslator = async (
   playlistsGroup: IPlaylistList
 ) => {
-  try{
-  const playlistsNameArray: string[] = [];
-  playlistsGroup.playlists.forEach((playlist: IPlaylist) => {
-    playlistsNameArray.push(playlist.name);
-  });
-  const combinedNameString = playlistsNameArray.join("|,%|");
+  try {
+    const playlistsNameArray: string[] = [];
+    playlistsGroup.playlists.forEach((playlist: IPlaylist) => {
+      playlistsNameArray.push(playlist.name);
+    });
+    const combinedNameString = playlistsNameArray.join("|,%|");
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/textTranslator?originalText=${combinedNameString}`
-  );
-  const combinedTranslatedName = await res.json();
-  console.log(combinedTranslatedName, "combinedTranslatedName");
-  const translatedNames = combinedTranslatedName.text.split("|,%|");
-  playlistsGroup.playlists.forEach((playlist: IPlaylist, index: number) => {
-    playlist.name = translatedNames[index];
-  });
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/textTranslator?originalText=${combinedNameString}`
+    );
+    const combinedTranslatedName = await res.json();
+    console.log(combinedTranslatedName, "combinedTranslatedName");
+    const translatedNames = combinedTranslatedName.text.split("|,%|");
+    playlistsGroup.playlists.forEach((playlist: IPlaylist, index: number) => {
+      playlist.name = translatedNames[index];
+    });
 
-  return playlistsGroup;
-}catch{
-  return playlistsGroup;
-}
+    return playlistsGroup;
+  } catch {
+    return playlistsGroup;
+  }
 };
