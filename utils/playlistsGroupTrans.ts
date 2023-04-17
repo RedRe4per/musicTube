a9e24a62178f73d6cc003d04d603506850f5e4db
RedRe4per <s3582474@gmail.com>
2023-04-18 02:16:29 +1000

    fix: string seperator

--- utils/playlistsGroupTrans.ts
+++ utils/playlistsGroupTrans.ts
@@ -8,14 +8,14 @@ export const playlistsGroupTranslator = async (
     playlistsGroup.playlists.forEach((playlist: IPlaylist) => {
       playlistsNameArray.push(playlist.name);
     });
-    const combinedNameString = playlistsNameArray.join("|$|");
+    const combinedNameString = playlistsNameArray.join("|###|");
 
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/textTranslator?originalText=${combinedNameString}`
     );
     const combinedTranslatedName = await res.json();
 
-    const translatedNames = combinedTranslatedName.text.split("|$|");
+    const translatedNames = combinedTranslatedName.text.split("|###|");
     playlistsGroup.playlists.forEach((playlist: IPlaylist, index: number) => {
       playlist.name = translatedNames[index];
     });
