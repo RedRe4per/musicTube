6a64c457c63517cd0ef4994ee2d16e5d85940791
RedRe4per <s3582474@gmail.com>
2023-04-18 02:33:35 +1000

    fix: alternative group translation method

--- utils/playlistsGroupTrans.ts
+++ utils/playlistsGroupTrans.ts
@@ -4,22 +4,9 @@ export const playlistsGroupTranslator = async (
   playlistsGroup: IPlaylistList
 ) => {
   try {
-    const playlistsNameArray: string[] = [];
     playlistsGroup.playlists.map(async (playlist: IPlaylist) => {
       playlist.name = await translateToEng(playlist.name);
-      //playlistsNameArray.push(playlist.name);
     });
-    // const combinedNameString = playlistsNameArray.join("|###|");
-
-    // const res = await fetch(
-    //   `${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/textTranslator?originalText=${combinedNameString}`
-    // );
-    // const combinedTranslatedName = await res.json();
-
-    // const translatedNames = combinedTranslatedName.text.split("|###|");
-    // playlistsGroup.playlists.forEach((playlist: IPlaylist, index: number) => {
-    //   playlist.name = translatedNames[index];
-    // });
     return playlistsGroup;
   } catch {
     return playlistsGroup;
@@ -35,5 +22,36 @@ export const translateToEng = async (originalText: string) => {
     return engText.text;
   } catch {
     return originalText;
+  }
+};
+
+/*
+This method is temporarily deprecated. It often reports errors when deployed 
+in Vercel. But it can solve the problem of some translation api sending limit.
+*/
+export const playlistsGroupTranslator2 = async (
+  playlistsGroup: IPlaylistList
+) => {
+  try {
+    const playlistsNameArray: string[] = [];
+    playlistsGroup.playlists.map(async (playlist: IPlaylist) => {
+      playlistsNameArray.push(playlist.name);
+    });
+    
+    const combinedNameString = playlistsNameArray.join("|###|");
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/textTranslator?originalText=${combinedNameString}`
+    );
+    const combinedTranslatedName = await res.json();
+
+    const translatedNames = combinedTranslatedName.text.split("|###|");
+    playlistsGroup.playlists.forEach((playlist: IPlaylist, index: number) => {
+      playlist.name = translatedNames[index];
+    });
+
+    return playlistsGroup;
+  } catch {
+    return playlistsGroup;
   }
 };
