a0d31070bf3bcc4d2789846f142f09213d38f17f
RedRe4per <s3582474@gmail.com>
2023-04-17 23:42:59 +1000

    fix: translator

--- utils/playlistsGroupTrans.ts
+++ utils/playlistsGroupTrans.ts
@@ -13,7 +13,7 @@ export const playlistsGroupTranslator = async (
     `${process.env.NEXT_PUBLIC_CLIENT_ADDRESS}/api/textTranslator?originalText=${combinedNameString}`
   );
   const combinedTranslatedName = await res.json();
-  console.log(combinedTranslatedName, "combinedTranslatedName")
+  console.log(combinedTranslatedName, "combinedTranslatedName");
   const translatedNames = combinedTranslatedName.text.split("|,%|");
   playlistsGroup.playlists.forEach((playlist: IPlaylist, index: number) => {
     playlist.name = translatedNames[index];
