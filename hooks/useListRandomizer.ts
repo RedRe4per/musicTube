f1db5e37984801d2efde3c60344741364a7f93b5
RedRe4per <s3582474@gmail.com>
2023-04-20 05:24:51 +1000

    feat: list randomizer

--- hooks/useListRandomizer.ts
+++ hooks/useListRandomizer.ts
@@ -5,18 +5,20 @@ import { shuffle } from "@/utils/shuffle";
 import { IMusicDetail } from "@/interfaces/music";
 
 export const useListRandomizer = () => {
-    const { currentMusic } = useContext(PlayAndPauseContext );
-    const { playerList, setPlayerList} = useContext(PlayerContext);
+  const { currentMusic } = useContext(PlayAndPauseContext);
+  const { playerList, setPlayerList } = useContext(PlayerContext);
 
-    const handleListRandomizer = () => {
-        if(playerList.length < 2) return;
-        const listWithoutCurrentMusic = playerList.filter((musicItem: IMusicDetail)=>{
-            return musicItem.id !== currentMusic?.id;
-        })
-        const shuffleList = shuffle(listWithoutCurrentMusic);
-        const randomizedList = [currentMusic, ...shuffleList] as IMusicDetail[];
-        setPlayerList(randomizedList);
-    }
+  const handleListRandomizer = () => {
+    if (playerList.length < 2) return;
+    const listWithoutCurrentMusic = playerList.filter(
+      (musicItem: IMusicDetail) => {
+        return musicItem.id !== currentMusic?.id;
+      }
+    );
+    const shuffleList = shuffle(listWithoutCurrentMusic);
+    const randomizedList = [currentMusic, ...shuffleList] as IMusicDetail[];
+    setPlayerList(randomizedList);
+  };
 
-    return {handleListRandomizer}
-}
+  return { handleListRandomizer };
+};
--- utils/shuffle.ts
+++ utils/shuffle.ts
@@ -1,16 +1,15 @@
 export function shuffle<T>(array: T[]): T[] {
-    let currentIndex = array.length;
-    let temporaryValue: T;
-    let randomIndex: number;
-  
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
+  let currentIndex = array.length;
+  let temporaryValue: T;
+  let randomIndex: number;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
-  
+  return array;
+}
